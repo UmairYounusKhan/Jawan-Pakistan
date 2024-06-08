e97f0c53ff6a6d8a4271ee18a207b391b23979f5
UmairYounusKhan <159928842+UmairYounusKhan@users.noreply.github.com>
2024-06-08 14:32:49 +0500

    Add files via upload

---
+++ Module B Section A/Class 1 TASK (Table)/app.js
@@ -0,0 +1,8 @@
+var input1 = +prompt("Emter your table ");
+var input2 = +prompt("Emter your table Limit");
+
+for (var i = 0; i <= input2; i++) {
+    document.write(input1 + " x " + i + " = " + input1 * i 
+    + "<br>")
+}
+
